8c3b6af10859a4562f882b881c9529304d05f0b4
h-morozumi <morozumi.h@outlook.com>
2023-11-14 08:44:22 +0000

    ひな型作成

---
+++ dalle-game/pages/games/[id].vue
@@ -0,0 +1,10 @@
+<template>
+    GameID : {{ id }}
+</template>
+  
+<script lang="ts" setup>
+  import { useRoute } from 'vue-router';
+  
+  const route = useRoute();
+  const id = route.params.id as string;
+</script>
