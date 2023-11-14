55004035b25148659ac645ccd2a388e647865697
h-morozumi <morozumi.h@outlook.com>
2023-11-14 10:55:47 +0000

    Room作成

--- dalle-game/pages/games/[id].vue
+++ dalle-game/pages/games/[id].vue
@@ -1,10 +1,29 @@
 <template>
-    GameID : {{ id }}
+    GameID : {{ id }} : <UButton icon="i-heroicons-clipboard-document" color="gray" @click="copyToClipboard">Game ROOM URLをコピー</UButton>
 </template>
   
 <script lang="ts" setup>
   import { useRoute } from 'vue-router';
+  import { ref,computed  } from 'vue';
   
   const route = useRoute();
   const id = route.params.id as string;
+
+  // 現在のページの完全なURLを取得
+  const currentUrl = computed(() => {
+    return window.location.origin + route.fullPath;
+  });
+  
+  // クリップボードに値をコピーする関数
+  const copyToClipboard = async () => {
+    if (navigator.clipboard) {
+      // Clipboard APIを使用してテキストをクリップボードにコピー
+      try {
+        await navigator.clipboard.writeText(currentUrl.value);
+        console.log('Copied to clipboard');
+      } catch (err) {
+        console.error('Failed to copy: ', err);
+      }
+    }
+  };
 </script>
--- dalle-game/pages/index.vue
+++ dalle-game/pages/index.vue
@@ -1,3 +1,17 @@
 <template>
-    <h1>index.vue</h1>
+    <UCard>
+      <template #header>
+        <Placeholder class="h-8" />
+        DALL-E Game
+      </template>
+  
+      <Placeholder class="h-32" />
+      <UButton @click="createGame">New Game!</UButton>
+    </UCard>
 </template>
+
+<script setup>
+const createGame = () => {
+  navigateTo('/games/11467')
+}
+</script>
