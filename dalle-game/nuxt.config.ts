919facc1f43f6584b5497d26b86a5d956eebccb7
h-morozumi <morozumi.h@outlook.com>
2023-11-22 10:30:10 +0000

    画像比較の検証

--- dalle-game/nuxt.config.ts
+++ dalle-game/nuxt.config.ts
@@ -1,5 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxt/ui"]
+  modules: ["@nuxt/ui"],
+  runtimeConfig: {
+    // 公開可能な設定
+    public: {
+      apiBase: process.env.API_BASE ,
+      // 他の公開環境変数
+    },
+    // サーバーサイド限定の設定
+    apiSecret: process.env.API_SECRET,
+    // 他の秘密の環境変数
+  }
 })
--- dalle-game/server/api/azure.get.ts
+++ dalle-game/server/api/azure.get.ts
@@ -1,101 +1,109 @@
-import { defineEventHandler, sendError, createError, getQuery } from 'h3';
+import { defineEventHandler, createError, getQuery } from 'h3';
+import { useRuntimeConfig } from '#imports'
 import fetch from 'node-fetch';
-import sharp from 'sharp';
 
+const config = useRuntimeConfig();
+// 比較対象の画像
+const url1 = 'https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2Fbs8z1i%2FbtroWDmAcWU%2F180Dv1tWj54jk0nWpL6gG1%2Fimg.jpg';
+// AI Vision
+const endpoint = config.public.apiBase; // AI Vision Endpoint URL
+const key = config.apiSecret; // AI Vision API Key
+const apiUrl = `${endpoint}computervision/retrieval:vectorizeImage?api-version=2023-02-01-preview&modelVersion=latest`;
 
 export default defineEventHandler(async (event) => {
 
+    try {
+        // パラメータから画像URLを取得
         const query = await getQuery(event);
         const url = query.url as string | undefined;
         if (!url) {
           throw createError({ statusCode: 400, statusMessage: 'No URL provided' });
         }
 
-    //画像1URL
-    const url1 = 'https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2Fbs8z1i%2FbtroWDmAcWU%2F180Dv1tWj54jk0nWpL6gG1%2Fimg.jpg';
-    //画像2URL
-    // const url2 = 'https://tryxtrip.com/wp-content/uploads/2015/08/3006983_m.jpg';
-    const url2 = 'https://img-s-msn-com.akamaized.net/tenant/amp/entityid/AA1kcIad.img?w=1920&h=1080&q=60&m=2&f=jpg';
-    
-    // 画像1を取得
-    const response1 = await fetch(url1);
-    if (!response1.ok) {
-        throw createError({ statusCode: response1.status, statusMessage: response1.statusText });
+        // 画像を取得
+        const image1 = await fetchAndProcessImage(url1);
+        const iamge2 = await fetchAndProcessImage(url);
+
+        // 画像をAzure Visionに投げてベクトルを取得
+        const vector1 = await postToAzureVision(image1);
+        const vector2 = await postToAzureVision(iamge2);
+
+        // 類似度を計算
+        const similarity = getCosineSimilarity(vector1.vector, vector2.vector);
+        console.log(similarity)
+        return { similarity };
+    } catch (error) {
+        console.error(error);
+        throw createError({ statusCode: 500, statusMessage: 'Internal Server Error' });
     }
-    // 画像2を取得
-    const response2 = await fetch(url);
-    if (!response2.ok) {
-        throw createError({ statusCode: response2.status, statusMessage: response2.statusText });
-    }
-
-    // AI Vision Endpoint URL
-    const endpoint = "";
-    // AI Vision API Key
-    const key = "";
-
-    // 画像1のバイナリデータを取得して、下記APIのリクエストボディに設定する
-    const buffer1 = await response1.arrayBuffer();
-    // バイナリデータをボディに設定してPOSTクエリを実行
-    const resAzure1 = await fetch(`${endpoint}/computervision/retrieval:vectorizeImage?api-version=2023-02-01-preview&modelVersion=latest`, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/octet-stream",
-            "Ocp-Apim-Subscription-Key": key
-        },
-        body: Buffer.from(buffer1)
-    });
-    // 画像2のバイナリデータを取得して、下記APIのリクエストボディに設定する
-    const buffer2 = await response2.arrayBuffer();
-    // バイナリデータをボディに設定してPOSTクエリを実行
-    const resAzure2 = await fetch(`${endpoint}/computervision/retrieval:vectorizeImage?api-version=2023-02-01-preview&modelVersion=latest`, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/octet-stream",
-            "Ocp-Apim-Subscription-Key": key
-        },
-        body: Buffer.from(buffer2)
-    });
-
-    // //画像1のURLをPOSTで呼び出す
-    // const resAzure = await fetch(`${endpoint}/computervision/retrieval:vectorizeImage?api-version=2023-02-01-preview&modelVersion=latest`, {
-    //     method: "POST",
-    //     headers: {
-    //         "Content-Type": "application/json",
-    //         "Ocp-Apim-Subscription-Key": key
-    //     },
-    //     body: JSON.stringify({
-    //         url: url1
-    //     })
-    // });
-
-    // レスポンスから結果を取り出す
-    const result1 = await resAzure1.json() as AzureResponse;
-    const result2 = await resAzure2.json() as AzureResponse;
-
-    // 画像1と画像2の類似度を判別
-    const similarity = getCosineSimilarity(result1.vector, result2.vector);
-
-
-    console.log(similarity)
-
-    return { similarity};
 });
 
-interface AzureResponse {
-    modelVersion:string;
-    vector: number[];
-  }
-
-function getCosineSimilarity(vector1: number[], vector2: number[]): number {
-    let dotProduct = 0;
-    const length = Math.min(vector1.length, vector2.length);
-    for (let i = 0; i < length; i++) {
-      dotProduct += vector1[i] * vector2[i];
+// Fetch an image from a URL and get its vector representation
+async function fetchAndProcessImage(imageUrl: string): Promise<ArrayBuffer> {
+    const response = await fetch(imageUrl);
+    if (!response.ok) {
+      throw createError({ statusCode: response.status, statusMessage: response.statusText });
     }
-    
+    const buffer = await response.arrayBuffer();
+    return buffer;
+}
+
+// Post binary data to Azure Vision and get the vector response
+async function postToAzureVision(buffer: ArrayBuffer): Promise<AzureResponse> {
+ 
+    const response = await fetch(apiUrl, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/octet-stream',
+        'Ocp-Apim-Subscription-Key': key,
+      },
+      body: Buffer.from(buffer),
+    });
+  
+    const azureResponse = (await response.json()) as AzureResponse;
+    return azureResponse;
+}
+
+// Post url to Azure Vision and get the vector response
+async function postUrlToAzureVision(url: string): Promise<AzureResponse> {
+  
+    const response = await fetch(apiUrl, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Ocp-Apim-Subscription-Key": key
+        },
+        body: JSON.stringify({
+            url: url
+        })
+    });
+    const azureResponse = (await response.json()) as AzureResponse;
+    return azureResponse;
+}
+interface AzureResponse {
+    modelVersion: string;
+    vector: number[];
+}
+
+// Calculate the cosine similarity between two vectors
+function getCosineSimilarity(vector1: number[], vector2: number[]): number {
+    const dotProduct = vector1.reduce((sum, value, i) => sum + value * vector2[i], 0);
     const magnitude1 = Math.sqrt(vector1.reduce((sum, x) => sum + x * x, 0));
     const magnitude2 = Math.sqrt(vector2.reduce((sum, x) => sum + x * x, 0));
-  
     return dotProduct / (magnitude1 * magnitude2);
-  }
+}
+
+// // 類似度を計算する関数S
+// function getCosineSimilarity(vector1: number[], vector2: number[]): number {
+//     let dotProduct = 0;
+//     const length = Math.min(vector1.length, vector2.length);
+//     for (let i = 0; i < length; i++) {
+//       dotProduct += vector1[i] * vector2[i];
+//     }
+    
+//     const magnitude1 = Math.sqrt(vector1.reduce((sum, x) => sum + x * x, 0));
+//     const magnitude2 = Math.sqrt(vector2.reduce((sum, x) => sum + x * x, 0));
+  
+//     return dotProduct / (magnitude1 * magnitude2);
+//   }
   
